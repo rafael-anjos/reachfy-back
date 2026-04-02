fcc9e867ac0f98146cd81848d4a1081f87d2f934
Rafael Anjos <anjosrafael59@gmail.com>
2026-04-01 22:24:00 -0300

    configurando cors

--- src/main/java/com/project/social/infra/security/SecurityConfig.java
+++ src/main/java/com/project/social/infra/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://www.reachfy.com.br"));
+        configuration.setAllowedOrigins(Arrays.asList("https://www.reachfy.com.br", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
